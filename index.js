e0b9f504293ba935ab981384ced5134b125bb95c
Zack Kollar <rallokkcaz@gmail.com>
2018-02-18 10:10:22 -0800

    Made targetPath to work slightly differently.

--- index.js
+++ index.js
@@ -1,49 +1,56 @@
-var spawn = require('child_process').spawn;
+const spawn = require('child_process').spawn
 
-module.exports = function(repo, targetPath, opts, cb) {
-
+module.exports = (repo, targetPath, opts, cb) => {
     if (typeof opts === 'function') {
-        cb = opts;
-        opts = null;
+        cb = opts
+        opts = null
     }
 
-    opts = opts || {};
+    opts = opts || {}
 
-    var git = opts.git || 'git';
-    var args = ['clone'];
+    const git = opts.git || 'git'
+    const args = []
+
+    args.push('-C')
+    args.push(targetPath || './')
+
+    args.push('clone')
 
     if (opts.shallow) {
-        args.push('--depth');
-        args.push('1');
+        args.push('--depth')
+        args.push('1')
     }
 
-    args.push('--');
-    args.push(repo);
-    args.push(targetPath);
+    args.push(repo)
 
-    var process = spawn(git, args);
+    const process = spawn(git, args)
+
+    process.stderr.on('data', (data) => {
+        console.log(`Git output:\n${data}`)
+    })
+
     process.on('close', function(status) {
         if (status == 0) {
             if (opts.checkout) {
-                _checkout();
+                _checkout()
             } else {
-                cb && cb();    
+                cb && cb()    
             }
         } else {
-            cb && cb(new Error("'git clone' failed with status " + status));
+            cb && cb(new Error("'git clone' failed with status " + status))
         }
-    });
+    })
 
     function _checkout() {
-        var args = ['checkout', opts.checkout];
-        var process = spawn(git, args, { cwd: targetPath });
+        const args = ['checkout', opts.checkout]
+        const process = spawn(git, args, { cwd: targetPath })
         process.on('close', function(status) {
             if (status == 0) {
-                cb && cb();
+                cb && cb()
             } else {
-                cb && cb(new Error("'git checkout' failed with status " + status));
+                cb && cb(new Error("'git checkout' failed with status " + status))
             }
-        });
+        })
     }
 
 }
--- test/test.js
+++ test/test.js
@@ -1,8 +1,10 @@
-var gitClone = require('../index');
+var gitClone = require('../index')
 
-gitClone('git@github.com:jaz303/tpl-simple-site.git', './test-checkout', {
-	checkout: 'a76362b0705d4126fa4462916cabb2506ecfe8e2' },
+gitClone(
+	'https://github.com/SeedyROM/test_document',
+	'./',
+	{},
 	function(err) {
-		console.log("complete!");
-		console.log(err);
-	});
+		console.log(err)
+	}
+)
